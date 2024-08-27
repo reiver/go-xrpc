4222059b9013c29c44c8351b15e1487bdcfac383
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-08-26 21:29:20 -0700

    scheme docs

--- scheme.go
+++ scheme.go
@@ -1,6 +1,6 @@
 package xrpc
 
 const (
-	Scheme = "xrpc"
-	SchemeUnencrypted = Scheme + "-unencrypted"
+	Scheme            = "xrpc"             // example: "xrpc://public.api.bsky.app/app.bsky.actor.getProfile?actor=reiver.bsky.social"
+	SchemeUnencrypted = "xrpc-unencrypted" // example: "xrpc-unencrypted://localhost/app.bsky.actor.getProfile?actor=reiver.bsky.social"
 )
