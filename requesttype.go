bd28e5ce9b78f0b6c98a10888f883edc5dd23816
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-08-31 09:40:20 -0700

    corrected spelling

--- requesttype.go
+++ requesttype.go
@@ -1,7 +1,7 @@
 package xrpc
 
 const (
-	RequstTypeExecute    string = "execute" // an alternative name for "procedure" so that all the request-types can be a verb.
+	RequestTypeExecute   string = "execute" // an alternative name for "procedure" so that all the request-types can be a verb.
 	RequestTypeProcedure string = "procedure"
 	RequestTypeQuery     string = "query"
 	RequestTypeSubscribe string = "subscribe"
