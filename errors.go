c764ddcdbeb99255438bacab63ac45ce99ab6edd
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-08-31 06:31:56 -0700

    xrpc.SetUserAgent()

--- errors.go
+++ errors.go
@@ -8,6 +8,8 @@ const (
 	errClosed                 = erorr.Error("xrpc: closed")
 	errEmptyHost              = erorr.Error("xrpc: empty host")
 	errNilDestination         = erorr.Error("xrpc: nil destination")
+	errNilHTTPRequest         = erorr.Error("xrpc: nil http-request")
+	errNilHTTPRequestHeader   = erorr.Error("xrpc: nil http-request header")
 	errNilHTTPResponse        = erorr.Error("xrpc: nil http-response")
 	errNilHTTPResponseBody    = erorr.Error("xrpc: nil http-response body")
 	errNilReader              = erorr.Error("xrpc: nil reader")
